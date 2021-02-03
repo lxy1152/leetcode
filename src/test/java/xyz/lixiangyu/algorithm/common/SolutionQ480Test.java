4a2691d06c6304c2cf75ba81121a8b669063c285
Xiangyu Li <2676092082@qq.com>
2021-02-03 22:29:11 +0800

    480. 滑动窗口中位数
    480. 滑动窗口中位数(优先队列 大顶堆小顶堆)

---
+++ src/test/java/xyz/lixiangyu/algorithm/common/SolutionQ480Test.java
@@ -0,0 +1,41 @@
+package xyz.lixiangyu.algorithm.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+import xyz.lixiangyu.algorithm.AbstractTest;
+
+import java.util.Arrays;
+
+/**
+ * {@link SolutionQ480}的测试类
+ *
+ * @author lixiangyu
+ */
+public class SolutionQ480Test extends AbstractTest {
+    @Override
+    public void beforeTestMethod() {
+        super.beforeTestMethod();
+        solution = new SolutionQ480();
+    }
+
+    @Override
+    public void testSolutionCaseOne() {
+        Assert.assertEquals(
+                Arrays.toString(new double[]{1, -1, -1, 3, 5, 6}),
+                Arrays.toString((double[]) solution.solve(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+    }
+
+    @Test
+    public void testSolutionCaseTwo() {
+        Assert.assertEquals(
+                Arrays.toString(new double[]{2147483647.0}),
+                Arrays.toString((double[]) solution.solve(new int[]{2147483647, 2147483647}, 2)));
+    }
+
+    @Test
+    public void testSolutionCaseThree() {
+        Assert.assertEquals(
+                Arrays.toString(new double[]{1.0, -1.0, -1.0, 3.0, 5.0, 6.0}),
+                Arrays.toString((double[]) solution.solve(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+    }
+}
