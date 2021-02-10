67ae99e0f5e0213a88a3c8514462111a392260c6
Xiangyu Li <2676092082@qq.com>
2021-02-10 20:03:57 +0800

    383. 赎金信
    383. 赎金信(数组遍历)

---
+++ src/test/java/xyz/lixiangyu/algorithm/common/SolutionQ383Test.java
@@ -0,0 +1,40 @@
+package xyz.lixiangyu.algorithm.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+import xyz.lixiangyu.algorithm.AbstractTest;
+import xyz.lixiangyu.algorithm.constants.Constants;
+
+/**
+ * {@link SolutionQ383}的测试类
+ *
+ * @author lixiangyu
+ */
+public class SolutionQ383Test extends AbstractTest {
+    @Override
+    public void beforeTestMethod() {
+        super.beforeTestMethod();
+        solution = new SolutionQ383();
+        solutionType = Constants.FAST_SOLUTION;
+    }
+
+    @Override
+    public void testSolutionCaseOne() {
+        Assert.assertEquals(true, solution.solve("aa", "aab", solutionType));
+    }
+
+    @Test
+    public void testSolutionCaseTwo() {
+        Assert.assertEquals(false, solution.solve("a", "b", solutionType));
+    }
+
+    @Test
+    public void testSolutionCaseThree() {
+        Assert.assertEquals(false, solution.solve("aa", "ab", solutionType));
+    }
+
+    @Test
+    public void testSolutionCaseFour() {
+        Assert.assertEquals(true, solution.solve("", "", solutionType));
+    }
+}
